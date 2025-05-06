3438b32ea9ab9d4577ba51d8cc027d23aa7def50
lrt16x <lthompson1@gmail.com>
2025-05-06 13:27:00 -0400

    Update app.py
    if __name__ == "__main__":
        app.run(debug=False, host="0.0.0.0", port=10000)

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def ocr_pdf():
     return send_file(tmp.name, as_attachment=True, download_name='ocr_output.txt', mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=10000)
+
